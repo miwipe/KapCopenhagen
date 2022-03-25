f0d320f8e18e13905deff6a9560e2490086f5b63
miwipe <npl206@a00815.science.domain>
2022-03-25 09:50:40 +0100

    Added code and data folder, as well as holi script

---
+++ code/holi.sh
@@ -0,0 +1,124 @@
+
+for infile in $(pwd)/*.fq
+do
+bname=$(basename $infile)
+echo $bname
+bname2=$(echo $bname | sed 's/.fq*/_holi/')
+basepath=$(pwd)/
+basefolder=$basepath
+echo $basepath
+echo $bname2
+mkdir $basepath$bname2
+cd $basepath$bname2
+pwd
+
+echo Step 1. Removing poly A tails
+fastq-grep -v "AAAAA$" ../$bname > kmer_$bname
+echo Step 2. Removing reverse complemented A tails
+fastq-grep -v "^TTTTT" kmer_$bname > kmer2_$bname
+echo Step 3. Removing rememnants adapter sequence 1 = AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC
+fastq-grep -v "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC" kmer2_$bname > adap1_kmer2_$bname
+echo Step 4. Removing remnants adapter sequence 2 = ATCTCGTATGCCGTCTTCTGCTTG
+fastq-grep -v "ATCTCGTATGCCGTCTTCTGCTTG" adap1_kmer2_$bname > adap2_kmer2_$bname
+
+
+echo Step 6. sga preprocessing
+sga preprocess --dust-threshold=1 -m 30 adap2_*.fq -o adap2_kmer2_$bname.pp.fq
+echo Step 7. sga index
+sga index --algorithm=ropebwt --threads=30 adap2_kmer2_$bname.pp.fq
+echo Step 8. sga filter
+sga filter --threads=30  --no-kmer-check adap2_kmer2_$bname.pp.fq -o adap2_kmer2_$bname.pp.rmdup.fq
+echo Step 9. Calculating read length distribution and outputting file
+cat adap2_kmer2_$bname.pp.rmdup.fq | awk '{if(NR%4==2) print length($1)}' | sort -n | uniq -c > adap2_kmer2_$bname.pp.rmdup.fq.read_length.txt
+
+ls -lh
+rm kmer*
+rm adap1*
+rm adap2_kmer2_$bname
+rm adap2_kmer2_$bname.pp.fq
+rm *wt
+rm *sai
+rm *rmdup.discard.fa
+
+for DB in /willerslev/datasets/ycw/norway_plant_ctgenoms/complete_genomes/final_db/norPlantCom.?
+do
+echo Mapping adap2_kmer2_$bname.pp.rmdup.fq against $DB
+nice -20 bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/ncbi_nt/nt.?
+do
+echo Mapping adap2_kmer2_$bname.pp.rmdup.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/refseq/vert_other/vert_other.?
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/refseq/vert_mam/vert_mam.?
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/refseq/vert_mam/vert_mam.??
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/refseq/invert/invert.?
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/refseq/microbe/viral_fungi_archaea.fa
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/microbial_dbs/GTDB/r89/bowtie2/gtdb_r89
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/ycw/polar_animals/arctic_animals.fa
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/ycw/polar_animals/arctic_animals_other.fa
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+for DB in /willerslev/datasets/ycw/polar_animals/arctic_animal_b3.fa
+do
+echo Mapping $bname.fq against $DB
+bowtie2 --threads 80 -k 1000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam
+done
+
+
+samtools merge --verbosity 5  $bname.merged.sam.gz *.bam -@ 60
+
+ls -lh *bam
+
+rm *nt.?*
+rm *vert_other.?*
+rm *vert_mam.?*
+rm *vert_mam.??*
+rm *invert.?*
+rm *norPlantCom*
+rm *viral_fungi_archaea*
+rm *arctic_animal*
+
+cd $basepath
+done
